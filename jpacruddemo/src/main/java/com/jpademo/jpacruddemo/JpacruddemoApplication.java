743d2e00f292d7806cfa2db417a7a0896f66589b
Rahul Chougule <rahulch07@Rahuls-MacBook-Pro.local>
2026-01-05 23:32:49 +0530

    initial commit

---
+++ jpacruddemo/src/main/java/com/jpademo/jpacruddemo/JpacruddemoApplication.java
@@ -0,0 +1,22 @@
+package com.jpademo.jpacruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JpacruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+			System.out.println("HELLO WORLD!!!");
+		};
+	}
+
+}
