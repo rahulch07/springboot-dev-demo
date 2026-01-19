e2e83027c1fa3d047be0c04e90cb774d1bee4429
Rahul Chougule <chouguler310@gmail.com>
2026-01-19 08:39:43 +0530

    added roles for users

--- cruddemo/src/main/java/com/example/cruddemo/security/DemoSecurityConfig.java
+++ cruddemo/src/main/java/com/example/cruddemo/security/DemoSecurityConfig.java
@@ -3,9 +3,13 @@ package com.example.cruddemo.security;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -17,5 +21,24 @@ public class DemoSecurityConfig {
         UserDetails susan = User.builder().username("susan").password("{noop}pass").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, marry, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasAllRoles("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasAllRoles("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasAllRoles("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasAllRoles("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasAllRoles("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasAllRoles("ADMIN")
+                );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
