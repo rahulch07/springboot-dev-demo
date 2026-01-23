28c89dfc87d034bf8d1a5791558ff93a77ced518
Rahul Chougule <chouguler310@gmail.com>
2026-01-23 09:00:35 +0530

    added request and model

--- mvcdemo/src/main/java/com/example/mvcdemo/controller/HelloWorldController.java
+++ mvcdemo/src/main/java/com/example/mvcdemo/controller/HelloWorldController.java
@@ -1,9 +1,14 @@
 package com.example.mvcdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Controller
 public class HelloWorldController {
@@ -17,5 +22,20 @@ public class HelloWorldController {
     @GetMapping("/processform")
     public String processform(){
         return "helloworld";    //view name
+    }
+
+    @GetMapping("/processform-v2")
+    public String processformV2(HttpServletRequest request, Model model){
+
+//        request.getParameterMap().forEach((key, value) ->{
+//            System.out.println("*&request**");
+//            System.out.println(key + "-" + Arrays.toString(value));
+//        });
+
+        String name = request.getParameter("studentName") + "-v2";
+
+        model.addAttribute("message", name);
+
+        return "helloworldV2";    //view name
     }
 }
